3accce3b37ae0f06a053d57c67f58ae8de36f430
Darkhan <dar_amangeldi@kbtu.kz>
2024-01-29 23:42:12 +0600

    lab2

---
+++ lab2/lab2_3Lists.py
@@ -0,0 +1,30 @@
+#Example
+mylist = ["apple", "banana", "cherry"]
+
+#Example
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+#Example
+thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+#Example
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+#Example
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+#Example
+list1 = ["abc", 34, True, 40, "male"]
+
+#Example
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+#Example
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
---
+++ lab2/lab2_6Dictionaries.py
@@ -0,0 +1,47 @@
+#Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+
+#Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["brand"])
+
+#Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
+
+#Example
+print(len(thisdict))
+
+#Example
+thisdict = {
+  "brand": "Ford",
+  "electric": False,
+  "year": 1964,
+  "colors": ["red", "white", "blue"]
+}
+
+#Example
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(type(thisdict))
+
+#Example
+thisdict = dict(name = "John", age = 36, country = "Norway")
+print(thisdict)
